b8b630839a37f6e36621342db69f63eb60f02172
naufalham <hambalinaufal4@gmail.com>
2024-11-22 14:31:44 +0700

    tambah templet

---
+++ application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	function index(){
+
+		// $this->load->model("Mmember");
+		// $data['jumlah_member_distrik'] = $this->Mmember->jumlah_member_distrik("header");
+
+
+		$this->load->view("home");
+	}
+}
