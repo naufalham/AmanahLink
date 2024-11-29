40ed3415b8633c3834790fb5d9b77921ab6e2317
anasarifin30 <anasarifin30@students.amikom.ac.id>
2024-11-29 11:19:50 +0700

    ubah nama web

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Marketplace</title>
+    <title>Admin AmanahLink</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.bootstrap5.css">
 </head>
