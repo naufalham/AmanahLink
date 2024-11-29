47ae692e09bffe81c3e9b00f6d6fcfbf8404545a
anasarifin30 <anasarifin30@students.amikom.ac.id>
2024-11-29 10:40:47 +0700

    nambah url buat asset

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = 'http://localhost/AmanahLink/';
 
 $config['assets_produk'] = $_SERVER["DOCUMENT_ROOT"]."/AmanahLink/assets/produk/";
-$config['url_produk'] = 'http://localhost/AmanahLink/produk/';
+$config['url_produk'] = 'http://localhost/AmanahLink/assets/produk/';
 
 /*
 |--------------------------------------------------------------------------
