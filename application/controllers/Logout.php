778c910c74e9d2956f6b8de493d513bbfcf52ffd
sherlymarsa <marsarenaaa@gmail.com>
2024-11-29 11:21:16 +0700

    Logout

---
+++ application/controllers/Logout.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Logout extends CI_Controller {
+
+	public function index()
+	{
+		//menghancurkan tiket biskop yang dibuat saat login tadi
+		$this->session->unset_userdata("id_admin");
+		$this->session->unset_userdata("username_admin");
+		//redirect ke halaman login
+		$this->session->set_flashdata('pesan_sukses', 'Anda telah Logout');
+		redirect('/','refresh');
+	}
+}
