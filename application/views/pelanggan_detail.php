2e8a246653a5bef6e1499812464c3bd9891bda56
sherlymarsa <marsarenaaa@gmail.com>
2024-12-02 17:37:38 +0700

    Pelanggan Punya Marsa

--- application/views/pelanggan_detail.php
+++ application/views/pelanggan_detail.php
@@ -29,35 +29,7 @@
 				</tr>
 			</table>
 		</div>
-<!-- 		<div class="col col-md-8">
-			<h5>Transaksi Jual</h5>
-			<table class="table table-bordered" id="tabelku">
-	            <thead>
-	                <tr>
-	                    <th>No</th>
-	                    <th>Tanggal</th>
-	                    <th>Total</th>
-	                    <th>Status</th>
-	                    <th>Opsi</th>
-	                </tr>
-	            </thead>
-	            <tbody>
-
-	                <?php foreach ($jual as $key => $value): ?>
-
-	                <tr>
-	                    <td><?php echo $key+1; ?></td>
-	                    <td><?php echo $value['tanggal_transaksi']; ?></td>
-	                    <td><?php echo $value['total_transaksi']; ?></td>
-	                    <td><?php echo $value['status_transaksi']; ?></td>
-	                    <td>
-	                        <a href="<?php echo base_url("transaksi/detail/".$value["id_transaksi"]) ?>" class="btn btn-info">Detail</a>
-	                    </td>
-	                </tr>
-	                <?php endforeach ?>
-	            </tbody>
-        </table>
-
+ 		<div class="col col-md-8">
 			<h5>Transaksi Beli</h5>
 			<table class="table table-bordered" id="tabelku">
 	            <thead>
@@ -75,7 +47,7 @@
 
 	                <tr>
 	                    <td><?php echo $key+1; ?></td>
-	                    <td><?php echo $value['tanggal_transaksi']; ?></td>
+	                    <td><?php echo $value['tgl_transaksi']; ?></td>
 	                    <td><?php echo $value['total_transaksi']; ?></td>
 	                    <td><?php echo $value['status_transaksi']; ?></td>
 	                    <td>
@@ -84,7 +56,7 @@
 	                </tr>
 	                <?php endforeach ?>
 	            </tbody>
-        </table> -->
+        </table>
 
 		</div>
 	</div>
