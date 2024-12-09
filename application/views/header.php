379d7b8adf583cba9c7983c6a14c451dfc4ea1a7
naufalham <hambalinaufal4@gmail.com>
2024-12-09 19:52:59 +0700

    hider

--- application/views/header.php
+++ application/views/header.php
@@ -8,79 +8,140 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <link href="https://fonts.googleapis.com/css2?family=Kantumruy+Pro:wght@400;500;700&display=swap" rel="stylesheet">
+    
     <style>
-      /* Add box shadow to navbar */
-      .navbar {
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Horizontal offset, vertical offset, blur radius, color */
-      }
-    </style>
+        body {
+            font-family: 'Kantumruy Pro', sans-serif;
+        }
 
+        .navbar {
+            font-family: 'Kantumruy Pro', sans-serif;
+            background-color: #ffffff;
+            border-bottom: 2px solid #DDDDDD;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2); /* Tambahkan bayangan */
+        }
+            
+        .navbar-brand img {
+            width: 150px; /* Sesuaikan ukuran logo */
+            height: auto;
+        }
+
+        /* Add box shadow to navbar */
+        .navbar {
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Horizontal offset, vertical offset, blur radius, color */
+        }
+        .navbar-nav {
+            flex-grow: 1;
+            justify-content: center;
+        }
+
+        .navbar-nav .nav-link {
+            font-family: 'Kantumruy Pro', sans-serif;
+            font-weight: 500; /* Membuat teks bold */
+            color: #006600;
+            font-size: 20px;
+            margin: 0 20px; /* Spasi antar menu */
+        }
+
+        .navbar-nav .nav-link:hover {
+            color: #004d00;
+        }
+
+        .navbar-nav.ms-auto .nav-link {
+            margin: 0 10px;
+        }
+
+        .navbar-toggler {
+            border: none;
+        }
+
+        .navbar-toggler-icon {
+            background-color: #006600;
+        }
+
+        .nav-item span {
+            border-left: 2px solid #F9C710;
+            height: 20px;
+            margin: 0 10px;
+            display: inline-block;
+        }       
+
+    </style>
   </head>
   <body>
-
-<nav class="navbar bg-body-tertiary">
-    <div class="container">
+    <!-- Membuat Navbar -->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
         <a class="navbar-brand" href="#">
-        <img src="assets/logo/logo_nav.png" width="125" height="34">
+          <img src="assets/logo/logo_nav.png" alt="Logo">
         </a>
-
-
-        <ul class="nav justify-content-center">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav me-auto">
             <li class="nav-item">
-                <a href="<?php echo base_url("beranda") ?>" class="nav-link link-success">Beranda</a>
+              <a href="<?php echo base_url('') ?>" class="nav-link">Beranda</a>
             </li>
             <li class="nav-item">
-                <a href="<?php echo base_url("produk") ?>" class="nav-link link-success">Produk</a>
+              <a href="<?php echo base_url('produk') ?>" class="nav-link">Produk</a>
             </li>
             <li class="nav-item">
-                <a href="<?php echo base_url("transaksi") ?>" class="nav-link link-success">Transaksi</a>
+              <a href="<?php echo base_url('transaksi') ?>" class="nav-link">Transaksi</a>
             </li>
-        </ul>
+          </ul>
 
-
-
-        <?php if ($this->session->userdata("id_member")): ?>
-        <ul class="nav justify-content-end ">
-            
+          <?php if ($this->session->userdata("id_pelanggan")): ?>                    
+          <ul class="navbar-nav ms-auto">
             <li class="nav-item">
+<<<<<<< HEAD
                 <a class="navbar-brand" href="#">
                     <img src="assets/logo/keranjang.png" width="28" height="25.67">
                 </a>
+=======
+              <a class="nav-link" href="#">
+                <img src="assets/logo/keranjang.png" alt="Keranjang" width="25">
+              </a>
+>>>>>>> 870846041fb7f962e1f0d9a6d52b12587a6d6209
             </li>
             <li class="nav-item">
-                <img src="assets/logo/_.png">
+              <a href="<?php echo base_url('akun') ?>" class="nav-link">
+                <?php echo $this->session->userdata('nama_pelanggan') ?>
+              </a>
             </li>
             <li class="nav-item">
+<<<<<<< HEAD
                 <a href="<?php echo base_url("akun") ?>" class="nav-link link-success">
                     <img src="assets/logo/profile.png" width="28" height="29">
                     <?php  echo $this->session->userdata("nama_member") ?>
                 </a>
+=======
+              <a href="<?php echo base_url('logout') ?>" class="nav-link">Logout</a>
+>>>>>>> 870846041fb7f962e1f0d9a6d52b12587a6d6209
             </li>
-        </ul>
-        <?php endif ?>
-
-        <?php if (!$this->session->userdata("id_member")): ?>
-            <ul class="nav justify-content-end align-items-center">
-                
-                    
-                <div class="btn">
-                    <a href="<?php echo base_url("login") ?>" class="btn btn-outline-success">Masuk</a>
-                </div>
+          </ul>
+          <?php endif; ?>
+          <?php if (!$this->session->userdata("id_pelanggan")): ?>
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <img src="assets/logo/_.png" width="3" height="25" top="34">
+                    <a href="<?php echo base_url('login') ?>" class="nav-link">Masuk</a>
                 </li>
-                <div class="btn">
-                    <a href="<?php echo base_url("register") ?>" class="btn btn-outline-success">Daftar</a>
-                </div>
+                <li class="nav-item" style="display: flex; align-items: center;">
+                    <span style="border-left: 3px solid #F9C710; height: 20px;"></span>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo base_url('register') ?>" class="nav-link">Daftar</a>
+                </li>
             </ul>
-        <?php endif ?>
+        <?php endif; ?>
+        </div>
+      </div>
+    </nav>
 
-
-    </div>
-
-
-</nav>
-                        
-
-                       
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
