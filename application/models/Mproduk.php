cc908850e98368f6c506ae04a03a120d12506175
naufalham <hambalinaufal4@gmail.com>
2024-12-09 18:59:12 +0700

    detail prpoduk lagi

--- application/models/Mproduk.php
+++ application/models/Mproduk.php
@@ -21,24 +21,21 @@ class Mproduk extends CI_Model {
 
 	
 
-	function detail($id_produk){
-		//detail data produk sesuai id_produk dan id_admin yang login
-		// $this->db->where('id_admin', $this->session->userdata("id_admin"));
-		$this->db->where('id_produk', $id_produk);
-		// $this->db->join("kategori", 'produk.id_kategori = kategori.id_kategori', 'left');
-		$q = $this->db->get("produk");
-		$d = $q->row_array();
+	function detail($id_produk) {
+        $this->db->where('id_produk', $id_produk);
+        $q = $this->db->get('produk');
+        $d = $q->row_array();
 
-		return $d;
-	}
+        return $d;
+    }
 
 
-	function detail_umum($id_produk){
-		$this->db->where('id_produk', $id_produk);
-		$this->db->join("kategori", 'produk.id_kategori = kategori.id_kategori', 'left');
-		$q = $this->db->get("produk");
-		$d = $q->row_array();
+	// function detail_umum($id_produk){
+	// 	$this->db->where('id_produk', $id_produk);
+	// 	$this->db->join("kategori", 'produk.id_kategori = kategori.id_kategori', 'left');
+	// 	$q = $this->db->get("produk");
+	// 	$d = $q->row_array();
 
-		return $d;
-	}
+	// 	return $d;
+	// }
 }
